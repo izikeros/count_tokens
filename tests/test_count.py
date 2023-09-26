e6e4ee204abe1d9d3af79cbc3b0ec34f162beca9
Krystian Safjan <ksafjan@gmail.com>
2023-09-26 12:08:40 +0200

    test: add tests

--- tests/test_count.py
+++ tests/test_count.py
@@ -2,6 +2,8 @@ import subprocess
 
 import pytest
 
+from count import count_tokens_in_file, count_tokens_in_string
+
 
 # TODO: KS: 2023-06-28: not implemented yet
 @pytest.mark.skip(reason="not implemented yet")
@@ -13,3 +15,12 @@ def test_count():
         text=True,
     )
     assert result.stdout.strip() == "67"
+
+
+# programmatic access
+def test_count_tokens_in_file():
+    assert count_tokens_in_file("doc.txt", "cl100k_base") == 67
+
+
+def test_count_tokens_in_string():
+    assert count_tokens_in_string("This is a test.", "cl100k_base") == 5
