10a6cdf6f7ff9357e11d3aa4bd6062701ca1d024
Krystian Safjan <ksafjan@gmail.com>
2023-09-26 12:47:20 +0200

    fix: fix exposing two functions

--- count_tokens/__init__.py
+++ count_tokens/__init__.py
@@ -1,2 +1,2 @@
-from .count_tokens import count_tokens_in_file, count_tokens_in_string
+from .count import count_tokens_in_file, count_tokens_in_string
 __all__ = ['count_tokens_in_file', 'count_tokens_in_string']
